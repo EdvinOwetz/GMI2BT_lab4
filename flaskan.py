5453ea7d9739c8aba6704aa9a1d89edaaa084322
EdvinOwetz <edvinowetz@gmail.com>
2022-02-23 15:00:30 +0100

    Continued development. Re-arranged some stuff and added some things.
    Co-authored-by: Nisslett <Nisslett@users.noreply.github.com>

--- flaskan.py
+++ flaskan.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 # from flask import request
 
 
@@ -8,10 +8,35 @@ app=Flask(__name__)
 
 #POST
 #@app.route("/",methods=["POST"])
-@app.route("/")
+@app.route("/form")
 def display_form():
-#    return "Hello"
     return render_template("index.html")
+
+@app.route("/submit",methods=["POST","GET"])
+def form_recive():
+    # lista=[]
+    # lista.append(request.form["fname"])
+    # lista.append(request.form["ename"])
+    # lista.append(request.form["kort"])
+    # text=""
+    # for item in lista:
+    #     text+=item+"\n"
+    # return text
+
+    return str(request.get_json())
+
+    #return render_template("return_form.html",data=data)
+    #return render_template('shortenurl.html', shortcode=request.form['shortcode'])
+
+
+# @app.route('/shortenurl', methods=['GET', 'POST'])
+# def shortenurl():
+#     if request.method == 'POST':
+#         return render_template('shortenurl.html', shortcode=request.form['shortcode'])
+#     elif request.method == 'GET':
+#         return 'A GET request was made'
+#     else:
+#         return 'Not a valid request method for this route'
 
 if(__name__=="__main__"):
     print("Starting Flask")
